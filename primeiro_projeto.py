cc4e17700ecc1c20390a73c86efc425a90ae6086
GabrielSartoriMosquer <gabriel.sartori.mosquer@gmail.com>
2025-11-06 16:23:04 -0300

    Commit Inicial

---
+++ primeiro_projeto.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+text_box = driver.find_element(by=By.NAME, value="my-text")
+text_box.send_keys("Selenium")
+
+
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+submit_button.click()
+
+sleep(5)
